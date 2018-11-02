3954e6e41c43a44fdf5fd23bff8e32536f1bb919
hubin <1547064892@qq.com>
2018-11-03 01:05:24 +0800

    2018.11.03

---
+++ RonftonCard.Service/AuthenController.cs
@@ -0,0 +1,51 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using RonftonCard.AuthenKey.RockeyArm;
+using RonftonCard.Common.AuthenKey;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BlueMoon;
+using System.Web;
+
+namespace RonftonCard.Service
+{
+	public class AuthenController : ApiController
+	{
+		protected static ILog logger = LogManager.GetLogger("RonftonCardService");
+
+		// GET api/authen 
+		[HttpGet]
+		public String Get()
+		{
+			String result = EnumerateKey();
+			logger.Debug(result);
+			return result;
+		}
+
+		// POST api/authen 
+		[HttpPost]
+		public IHttpActionResult Post()
+		{
+			int num = HttpContext.Current.Request.Form.Count;
+
+			logger.Debug("receive post" + num.ToString());
+
+			return Json<String>("I received request " + num.ToString());
+		}
+
+		private String EnumerateKey()
+		{
+			DongleKey key = new DongleKey();
+			AuthenKeyInfo[] keyInfo = key.Enumerate();
+
+			if (keyInfo.IsNullOrEmpty())
+				return "no key found !";
+
+			return JsonConvert.SerializeObject(keyInfo);
+		}
+	}
+}
---
+++ RonftonCard.Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RonftonCard.Service
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			log4net.Config.XmlConfigurator.Configure(new FileInfo(@"config\log4net.xml"));
+
+			string baseAddress = @"http://localhost:9000/";
+
+			// Start OWIN host 
+			using (WebApp.Start<StartService>(url: baseAddress))
+			{
+				Console.WriteLine("Any key to exit ...");
+				Console.ReadLine();
+			}
+		}
+	}
+}
