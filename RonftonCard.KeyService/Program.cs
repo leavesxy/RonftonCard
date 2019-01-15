ed8892926332917cf2190a8fa379069a7c7e2e91
hubin <1547064892@qq.com>
2019-01-15 17:43:02 +0800

    2019.01.15-2

--- RonftonCard.KeyService/Program.cs
+++ RonftonCard.KeyService/Program.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using Microsoft.Owin.Hosting;
 using RonftonCard.Core;
+using Bluemoon;
 
 namespace RonftonCard.KeyService
 {
 	class Program
 	{
-		private static String BASE_ADDRESS = @"http://localhost:9002/";
+		private static String DEFAULT_HOST_ADDRESS = @"http://localhost:9002/";
 		static void Main(string[] args)
 		{
-			ContextManager.Init("RonftonKeyService");
+            String host;
+
+            if (args.IsNullOrEmpty())
+                host = DEFAULT_HOST_ADDRESS;
+            else
+            {
+                host = args[0];
+                if (!host.EndsWith(@"/"))
+                    host += @"/";
+            }
+
+            ContextManager.Init("RonftonKeyService");
 			
 			// Start OWIN host 
-			using (WebApp.Start<StartService>(url: BASE_ADDRESS))
+			using (WebApp.Start<StartService>(url: host))
 			{
-				Console.WriteLine("Key service run OK!, " + BASE_ADDRESS);
+				Console.WriteLine("Key service run OK!, " + host);
 				HelperInfo.DbgHelperInfo();
 
 				Console.WriteLine("Any key to exit ...");
